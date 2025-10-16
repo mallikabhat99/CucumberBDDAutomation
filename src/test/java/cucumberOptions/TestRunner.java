d73e8db21b67769732d37b695c360f075198a9a2
mallikabhat99 <mallikabhat99@gmail.com>
2025-10-16 14:40:09 +0530

    final  - cucumber reoprts dont work

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/features",monochrome=true,dryRun=false,
-glue={"stepDefinations"}, tags = "@DeletePlace" , plugin ="json:target/jsonReports/cucumber-report.json")
+glue={"stepDefinations"}, tags = "@AddPlace" , plugin ="json:target/jsonReports/cucumber-report.json")
 public class TestRunner {
 
 }
